607bc82322991e02771a929aa427f9664d63cad3
Garrett Hale <gthkeno@gmail.com>
2022-05-27 21:29:07 -0500

    Funny human has guns and collision layers

---
+++ COPYRIGHT.md
@@ -0,0 +1,2 @@
+- [Human 3D model](https://opengameart.org/content/base-human-models-low-poly "CC-BY 3.0")
+  By Clint Bellanger (2009)
--- Raycaster.gd
+++ Raycaster.gd
@@ -1,33 +1,41 @@
 extends Spatial
 
+class_name PlayerGun
 
-onready var camera = $CameraPivot/Camera
-var rayOrigin = Vector3()
-var rayEnd = Vector3()
-
+onready var camera: Camera = $CameraPivot/Camera
+var rayOrigin := Vector3()
+var rayEnd := Vector3()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func _physics_process(_delta):
-	
-	var space_state = get_world().direct_space_state
-	
-	var mouse_position = get_viewport().get_mouse_position()
-	
+func _physics_process(_delta: float) -> void:
+	var space_state := get_world().direct_space_state
+	var mouse_position := get_viewport().get_mouse_position()
 	rayOrigin = camera.project_ray_origin(mouse_position)
-	
 	rayEnd = rayOrigin + camera.project_ray_normal(mouse_position) * 2000
-	
-	var intersection = space_state.intersect_ray(rayOrigin, rayEnd)
-	
-	var pos
-	
+	var intersection := space_state.intersect_ray(rayOrigin, rayEnd, [], 2)
+
+	var pos: Vector3
+
 	if not intersection.empty():
-		pos = intersection.position
+		print("not empty!", intersection.position)
+		pos = Vector3(intersection.position)
 	else:
 		pos = rayEnd
-	
-	($GunPivot as Spatial).look_at(Vector3(pos.x,pos.y,pos.z),Vector3(0,1,0))
-	
+
+	($GunPivot as Spatial).look_at(pos, Vector3(0,1,0))
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Click"):
+		pass
+
+var inViewport := false
+
+func _on_ViewportContainer_mouse_entered() -> void:
+	inViewport = true
+
+
+func _on_ViewportContainer_mouse_exited() -> void:
+	inViewport = false
