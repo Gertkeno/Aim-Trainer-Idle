944c9607fc7191bd68d0acd87fe4af85cec4cd27
Garrett Hale <gthkeno@gmail.com>
2022-06-04 20:57:46 -0500

    Save purchsable base costs for selling account

--- Player/SellAccountButton.gd
+++ Player/SellAccountButton.gd
@@ -8,8 +8,11 @@ var resetStatNames: Array = [
 	"respawnTime",
 	"totalPurchases",
 ]
-
 var resetStatValues: Array = []
+var buttonStatBaseCosts: Array = []
+
+export(int, 0, 100) var minimumPurchases = 2
+export(int, 1, 50) var doshPerPurchases = 3
 
 signal account_sold
 
@@ -19,6 +22,14 @@ func _ready() -> void:
 		resetStatValues.append(Stats.get(name))
 	Stats.connect("xpChanged", self, "_set_text")
 
+	buttonStatBaseCosts.resize(resetStatNames.size())
+	for button in get_tree().get_nodes_in_group("purchaseableButton"):
+		var b := button as Purchaseable
+		assert(b != null)
+		var index: int = resetStatNames.find(b.statName)
+		if index >= 0:
+			buttonStatBaseCosts[index] = b.baseCost
+
 func sell_account() -> void:
 	var potentialWorth := get_account_worth()
 	if potentialWorth > 0:
@@ -26,11 +37,20 @@ func sell_account() -> void:
 
 		for i in resetStatNames.size():
 			Stats.set(resetStatNames[i], resetStatValues[i])
-		emit_signal("account_sold", potentialWorth)
 
+		var buttons := get_tree().get_nodes_in_group("purchaseableButton")
+		for button in buttons:
+			var b := button as Purchaseable
+			assert(b != null)
+			var index: int = resetStatNames.find(b.statName)
+			if index >= 0:
+				b.baseCost = buttonStatBaseCosts[index]
+				b.update_text()
+		emit_signal("account_sold", potentialWorth)
+		_set_text()
 
 func get_account_worth() -> float:
-	return max(0, (Stats.totalPurchases - 1) / 10)
+	return max(0, (Stats.totalPurchases - minimumPurchases) / doshPerPurchases)
 
 func _set_text() -> void:
 	var worth := get_account_worth()
