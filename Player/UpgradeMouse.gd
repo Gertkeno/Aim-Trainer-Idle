a5c6b785064052517236941a1cc3481d64745154
Hankik <43581135+Hankik@users.noreply.github.com>
2022-06-05 14:51:16 -0400

    Mouse Button Changes Image Update
    -Pressing mouse button changes texture from a list of textures

---
+++ Player/UpgradeMouse.gd
@@ -0,0 +1,21 @@
+extends Button
+
+
+signal mouse_upgraded
+
+export(Array, Texture) var textures: Array
+onready var textureR := $TextureRect as TextureRect
+var index = -1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func _on_Button_pressed() -> void:
+	
+	if index < textures.size() - 1:
+		index += 1
+		emit_signal("mouse_upgraded")
+		textureR.texture = textures[index]
+	else:
+		index += 0
